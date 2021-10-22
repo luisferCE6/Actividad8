1ebfad335aaf29c0f725faa16471e13630ab3c98
Luis <luisferce6@gmail.com>
2021-10-21 20:12:38 -0500

    Nuevos cambios

--- src/actividad8/Actividad8.java
+++ src/actividad8/Actividad8.java
@@ -14,13 +14,31 @@ import java.util.Scanner;
 public class Actividad8 {
 
     public static void main(String[] args) throws InterruptedException {
-        
+       
+        Scanner entrada = new Scanner(System.in);
         Deck op = new Deck();
-        
-        op.shuffle();
-        op.head();      
-        op.pick();
-        op.hand();       
+        int x = 0;
+        do {
+            System.out.println("Poker \n" + "1 mezclar\n " + "2 Sacar carta\n " + "3 Carta alazar\n " + "4 mano de 5 cartas\n " + "5 salir\n ");
+            x = entrada.nextInt();
+
+            switch (x) {
+                case 1:
+                    op.shuffle();
+                    break;
+                case 2:
+                    op.head();
+                    break;
+                case 3:
+                    op.pick();
+                    break;
+                case 4:
+                    op.hand();
+                    break;
+                default:
+                    System.out.println("Gracias por jugar");
+            }
+        } while (x != 5);     
        
     }
 }
